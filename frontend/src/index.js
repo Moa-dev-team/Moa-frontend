ddf0d57bf4b3516615c5a33ecc8a21f5f4ce6155
Hosung <an3735297@pusan.ac.kr>
2023-02-27 17:25:29 +0900

    feat : test 환경 설정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TestGang from './TestGang';
+import TestSung from './TestSung';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TestGang/>
+    <TestSung/>
   </React.StrictMode>
 );
 
