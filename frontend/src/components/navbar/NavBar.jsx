1e9f9eff79593fdad08a4f93b15b3a256f5e7672
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-02-28 16:00:40 +0900

    feat : NavBar 컴포넌트 스타일링

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/Logo.png";
 import SmallButton from "../buttons/SmallButton";
+import styles from "./NavBar.module.css";
 
 const style = {
   color: "var(--color-blue)",
@@ -10,27 +11,35 @@ const style = {
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-          <h1>Moa</h1>
+    <header className={styles.header}>
+      <div className={styles.logoAndMenus}>
+        <Link to="/" className={styles.logo}>
+          <img className={styles.logoImage} src={logo} alt="logo" />
+          <h1 className={styles.title}>Moa</h1>
         </Link>
-        <NavLink
-          to="/projects"
-          style={({ isActive }) => (isActive ? style : {})}
-        >
-          프로젝트
-        </NavLink>
-        <NavLink
-          to="/studies"
-          style={({ isActive }) => (isActive ? style : {})}
-        >
-          스터디
-        </NavLink>
-        <NavLink to="/users" style={({ isActive }) => (isActive ? style : {})}>
-          유저
-        </NavLink>
+        <nav className={styles.menus}>
+          <NavLink
+            to="/projects"
+            className={styles.link}
+            style={({ isActive }) => (isActive ? style : {})}
+          >
+            프로젝트
+          </NavLink>
+          <NavLink
+            to="/studies"
+            className={styles.link}
+            style={({ isActive }) => (isActive ? style : {})}
+          >
+            스터디
+          </NavLink>
+          <NavLink
+            to="/users"
+            className={styles.link}
+            style={({ isActive }) => (isActive ? style : {})}
+          >
+            유저
+          </NavLink>
+        </nav>
       </div>
       <SmallButton />
     </header>
