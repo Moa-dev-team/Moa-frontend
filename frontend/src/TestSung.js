5f3958a6684a2ef4afbc6654a69351769037327a
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-02-28 15:08:10 +0900

    root 디자인 수정, 색상 변수 설정

--- frontend/src/TestSung.js
+++ frontend/src/TestSung.js
@@ -1,10 +1,12 @@
 import React from "react";
-import LargeButton from "./components/Buttons/LargeButton";
+import LargeButton from "./components/buttons/LargeButton";
 
 const TestSung = () => {
-  return <div style={{ marginTop: "10rem" }}>
-    <LargeButton text='자세히 보기' />
-  </div>;
+  return (
+    <div style={{ marginTop: "10rem" }}>
+      <LargeButton text="자세히 보기" />
+    </div>
+  );
 };
 
 export default TestSung;
