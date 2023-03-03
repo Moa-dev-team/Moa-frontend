23a554e7a019e4c3b448828a0306079403d8090f
Hosung <an3735297@pusan.ac.kr>
2023-03-03 16:03:25 +0900

    fix : MessagePage 에 userlist 와 chatlist 가 scroll되게 함

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -28,6 +28,60 @@ export default function Message() {
                     isSelected={false}
                     count="5"
                   />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
+                  <MessageUserProfile
+                    username="김재강"
+                    job="백엔드"
+                    isSelected={false}
+                    count="5"
+                  />
                 </div>
 
                 <hr className="d-block d-lg-none mt-1 mb-0" />
@@ -60,6 +114,21 @@ export default function Message() {
                     />
                     <ChatLeft
                       text="안녕못한데?"
+                      image={image}
+                      name="윤강"
+                      date={new Date("2020-01-01")}
+                    />
+                    <ChatRight
+                      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ducimus, reprehenderit, odit sed earum ullam impedit veritatis totam libero dolorum autem rem quia ipsa dolor nostrum? Suscipit, eius? Quo, aut.
+                      Quibusdam tempora dicta sint eum veritatis adipisci totam eos, deserunt commodi aperiam eaque laboriosam beatae! Libero tempore sunt odit deserunt, explicabo corporis labore adipisci mollitia autem ducimus, iusto consectetur cum.
+                      Qui, voluptate! Quae consequuntur deleniti veniam fugiat illo unde repellendus. Quis beatae tempora, aliquid quae illo praesentium ducimus magni velit nemo saepe odio id distinctio? Labore aspernatur impedit molestiae magni?"
+                      image={image}
+                      name="안호성"
+                      date={new Date("2020-01-01")}
+                    />
+                    <ChatLeft
+                      text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ducimus, reprehenderit, odit sed earum ullam impedit veritatis totam libero dolorum autem rem quia ipsa dolor nostrum? Suscipit, eius? Quo, aut.
+                      Quibusdam tempora dicta sint eum veritatis adipisci totam eos, deserunt commodi aperiam eaque laboriosam beatae! Libero tempore sunt odit deserunt, explicabo corporis labore adipisci mollitia autem ducimus, iusto consectetur cum?"
                       image={image}
                       name="윤강"
                       date={new Date("2020-01-01")}
