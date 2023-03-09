9738f6b1b4e785b4d500f3e70bfa5497afc0daa7
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-03-09 19:33:30 +0900

    feat : react-reveal 라이브러리를 다운하여 애니메이션 추가

--- frontend/src/components/home/HomeArticle.jsx
+++ frontend/src/components/home/HomeArticle.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LargeButton from "../buttons/LargeButton";
 import styles from "./HomeArticle.module.css";
 import { useNavigate } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 export default function HomeArticle({
   article: { title, text, path, position, image },
@@ -12,21 +13,25 @@ export default function HomeArticle({
   };
 
   return (
-    <article
-      className={styles.article}
-      style={{ backgroundImage: `url(${image})` }}
-    >
-      <div className={position === "left" ? styles.box : styles.box__right}>
-        <div className={position === "left" ? styles.info : styles.info__right}>
-          <h1 className={styles.title}>{title}</h1>
-          <pre
-            className={position === "left" ? styles.text : styles.text__right}
+    <Fade>
+      <article
+        className={styles.article}
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div className={position === "left" ? styles.box : styles.box__right}>
+          <div
+            className={position === "left" ? styles.info : styles.info__right}
           >
-            {text}
-          </pre>
-          <LargeButton text="자세히 보기" onClick={handleClick} />
+            <h1 className={styles.title}>{title}</h1>
+            <pre
+              className={position === "left" ? styles.text : styles.text__right}
+            >
+              {text}
+            </pre>
+            <LargeButton text="자세히 보기" onClick={handleClick} />
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </Fade>
   );
 }
