3ae4e8cf822b4b87e9771cdeb2ee90b9ab01171b
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-03-15 20:30:36 +0900

    feat : 프로젝트 카드 컴포넌트 생성 및 디자인

--- frontend/src/components/project/ProjectList.jsx
+++ frontend/src/components/project/ProjectList.jsx
@@ -1,5 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import ProjectCard from "./ProjectCard";
+import styles from "./ProjectList.module.css";
 
 export default function ProjectList() {
-  return <div>ProjectList</div>;
+  const {
+    isLoading,
+    error,
+    data: projects,
+  } = useQuery(["projects"], async () =>
+    fetch("/data/projects.json").then((res) => res.json())
+  );
+
+  return (
+    <>
+      {isLoading && <p>isLoading...</p>}
+      {error && <p>something is wrong!</p>}
+      {projects && (
+        <ul className={styles.list}>
+          {projects.data.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
