6441c3ae9d079a9f47d9d099d7b02f7c7b9cd00d
Hosung <an3735297@pusan.ac.kr>
2023-03-15 12:42:52 +0900

    Feature : New Project 틀 디자인, SmallButton fix
    SmallButton 에 배경과 텍스트 색을 지정하는 props 를 추가였습니다. 추가하지 않는다면 기존 설정한 값으로 보여서 기존 코드의 수정은 하지 않아도 될 것 같습니다.

--- frontend/src/components/Buttons/SmallButton.jsx
+++ frontend/src/components/Buttons/SmallButton.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./SmallButton.module.css";
 
-export default function SmallButton({ text, onClick, color }) {
+export default function SmallButton({ text, onClick, backgroundColor, textColor }) {
+  const color = {
+    backgroundColor : backgroundColor,
+    color : textColor,
+  }
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onClick} style={color}>
       {text}
     </button>
   );
--- frontend/src/pages/NewProject/NewProject.jsx
+++ frontend/src/pages/NewProject/NewProject.jsx
@@ -1,6 +1,76 @@
 import React from "react";
-// import styles from "./NewProject.module.css";
+import styles from "./NewProject.module.css";
+import SmallButton from "../../components/buttons/SmallButton";
 
 export default function NewProject() {
-  return <div>NewProject</div>;
+  return (
+    <div className="container m-5">
+      <form>
+        <div className="form-group row">
+          <label htmlFor="project" className={`col-form-label ${styles.title}`}>
+            프로젝트 제목
+          </label>
+          <div className="">
+            <input
+              type="text"
+              className={`form-control ${styles["input-title"]}`}
+              id="input1"
+              placeholder="프로젝트 제목을 입력하세요."
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className={`col-form-label ${styles.title}`}>
+            프로젝트 정보
+          </label>
+          <div className="container">
+            <div className="row">
+              <div className={styles["myCol-6"]}>1</div>
+              <div className={styles["myCol-6"]}>2</div>
+              <div className={styles["myCol-6"]}>3</div>
+              <div className={styles["myCol-6"]}>4</div>
+            </div>
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="introduce" className={`col-form-label ${styles.title}`}>
+            프로젝트 소개
+          </label>
+          <div className="">
+            <textarea
+              type="text"
+              className={`form-control ${styles["text-area"]}`}
+              id="introduce"
+              placeholder="프로젝트 대해 소개해주세요."
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="need" className={`col-form-label ${styles.title}`}>
+            요구사항
+          </label>
+          <div className="">
+            <textarea
+              type="text"
+              className={`form-control ${styles["text-area"]}`}
+              id="introduce"
+              placeholder="프로젝트 대해 소개해주세요."
+            />
+          </div>
+        </div>
+        <div className={styles["button-line"]}>
+          <div className="m-2">
+            <SmallButton
+              text={"취소"}
+              backgroundColor={"#D9D9D9"}
+              textColor={"black"}
+            />
+          </div>
+          <div className="m-2">
+            <SmallButton text={"글쓰기"}/>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
