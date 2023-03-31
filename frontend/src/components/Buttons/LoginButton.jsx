bcd38eb80acb6b8258d5b49c5311d41f6b8d5058
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-03-31 11:45:08 +0900

    feat: 로그인 페이지 모바일 버전 디자인

--- frontend/src/components/Buttons/LoginButton.jsx
+++ frontend/src/components/Buttons/LoginButton.jsx
@@ -4,10 +4,7 @@ import styles from "./LoginButton.module.css";
 export default function LoginButton({ sns }) {
   return (
     <li className={styles.list}>
-      <button className={styles.button}>
-        {sns.logo}
-        {/* <span className={styles.text}>{sns.text}</span> */}
-      </button>
+      <button className={styles.button}>{sns.logo}</button>
     </li>
   );
 }
