2435387e394cf2a26df25b2861c77e48d9b3a353
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-08-29 17:23:05 +0900

    feat: 프로필 수정 기능 구현

--- frontend2/src/components/templates/ProfileForm.jsx
+++ frontend2/src/components/templates/ProfileForm.jsx
@@ -1,41 +1,46 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import Select from "react-select";
-import { getUserProfile, setUserProfile } from "../../apis/profile";
 import Button from "../atoms/Button";
 import Box from "../atoms/Box";
 import Photo from "../molecules/Photo";
 import { CATEGORIES } from "../../utils/constant";
+import Input from "../molecules/Input";
+import useProfile from "../../hooks/useProfile";
 
 export default function ProfileForm() {
   const [isModifying, setIsModifying] = useState(true);
   const {
-    error,
-    data: { data },
-  } = useQuery(["profile"], getUserProfile);
-  const queryClient = useQueryClient();
-  const updateProfile = useMutation(
-    (data) => setUserProfile({ skills: data }),
-    {
-      onSuccess: () => queryClient.invalidateQueries(["profile"]),
-    }
-  );
+    profileQuery: {
+      error,
+      data: {
+        data: { name, skills, imageUrl, email },
+      },
+    },
+    updateProfile,
+  } = useProfile();
   const {
     control,
     register,
-    handleSubmit,
+    handleSubmit: onSubmit,
     formState: { isSubmitting, isSubmitted, errors },
   } = useForm({
     mode: "onSubmit",
     defaultValues: {
-      name: data.name,
-      email: data.email,
-      skills: data.skills.map((skill) => ({ value: skill, label: skill })),
+      name: name,
+      skills: skills.map((skill) => ({ value: skill, label: skill })),
     },
   });
 
-  const onSubmit = (formData) => {
+  const nameRegister = register("name", {
+    required: "이름은 필수 입력입니다.",
+    minLength: {
+      value: 2,
+      message: "2자리 이상의 이름을 사용하세요.",
+    },
+  });
+
+  const handleSubmit = (formData) => {
     const data = formData.skills.map((skill) => skill.value);
     updateProfile.mutate(data);
     setIsModifying(true);
@@ -45,48 +50,24 @@ export default function ProfileForm() {
     return <div>{error.message}</div>;
   }
   return (
-    <form id="user-form" onSubmit={handleSubmit(onSubmit)}>
-      <Photo
-        width="w-56"
-        className="rounded-full"
-        src={data.imageUrl}
-        alt={data.name}
-      />
+    <form id="user-form" onSubmit={onSubmit(handleSubmit)}>
+      <Photo width="w-56" className="rounded-full" src={imageUrl} alt={name} />
       <Box className="flex flex-col">
-        <label htmlFor="name">이름</label>
-        <input
+        <Input
           id="name"
           type="text"
           disabled={isModifying}
-          aria-invalid={
+          ariaInvalid={
             isSubmitted ? (errors.name ? "true" : "false") : undefined
           }
-          {...register("name", {
-            required: "이름은 필수 입력입니다.",
-            minLength: {
-              value: 2,
-              message: "2자리 이상의 이름을 사용하세요.",
-            },
-          })}
-        />
+          {...nameRegister}
+        >
+          이름
+        </Input>
         {errors.name && <small role="alert">{errors.name.message}</small>}
-        <label htmlFor="email">이메일</label>
-        <input
-          id="email"
-          type="text"
-          disabled={isModifying}
-          aria-invalid={
-            isSubmitted ? (errors.email ? "true" : "false") : undefined
-          }
-          {...register("email", {
-            pattern: {
-              value:
-                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-              message: "이메일 형식에 맞지 않습니다.",
-            },
-          })}
-        />
-        {errors.email && <small role="alert">{errors.email.message}</small>}
+        <Input id="email" type="email" disabled={true} value={email}>
+          이메일
+        </Input>
         <label htmlFor="skills">기술 스택</label>
         <Controller
           name="skills"
