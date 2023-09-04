322b847c556e74b6fbf6282bea11dddf910f4f58
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-09-04 15:30:59 +0900

    fix: 유저데이터가져오는 api 수정
    - get => post

--- frontend2/src/apis/user.js
+++ frontend2/src/apis/user.js
@@ -2,7 +2,7 @@ import { instance } from "./index";
 
 export const getUsers = async (cursor = null, limit = 20) => {
   const params = { cursor, limit };
-  const response = await instance.get("/global/members", { params });
+  const response = await instance.post("/global/members", { params });
 
   return response.data;
 };
